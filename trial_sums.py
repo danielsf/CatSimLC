27edb9420bf0d5b11fadca5c6d228c9f45c5072f
danielsf <scott.f.daniel@gmail.com>
2017-04-24 19:21:58 -0700

    add rough code to validate extirp
    again: mostly for my use during development

---
+++ trial_sums.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+def brute_sums(time_arr, f_arr, freq_arr):
+
+    sh = np.array([(f_arr*np.sin(2.0*np.pi*nu*time_arr)).sum() for nu in freq_arr])
+    ch = np.array([(f_arr*np.cos(2.0*np.pi*nu*time_arr)).sum() for nu in freq_arr])
+
+    return ch, sh
+
+
+def f_of_t(time_arr):
+    y = 0.1*np.sin(0.98*time_arr)
+    y += 0.35*np.cos(1.6*time_arr)
+    y += 0.77*np.sin(2.6*time_arr)
+    y += 0.45*np.cos(0.7*time_arr)
+    return y
+
+import time
+from fft import extirp_sums
+
+rng = np.random.RandomState(45)
+time_arr = rng.random_sample(5)*2.0*np.pi
+time_arr = np.sort(time_arr)
+fn_arr = f_of_t(time_arr)
+
+delta = (time_arr.max()-0.0)/1024.0
+
+t_start = time.time()
+cos_fft_sum, sin_fft_sum, ttk, hhk = extirp_sums(time_arr, fn_arr, delta)
+t_extirp = time.time()-t_start
+
+#hhk_control = f_of_t(ttk)
+
+#print('hhk residual %e ' % np.abs(hhk-hhk_control).max())
+#exit()
+
+freq_arr = np.array([k/(delta*len(cos_fft_sum)) for k in range(len(cos_fft_sum))])
+t_start = time.time()
+cos_brute, sin_brute = brute_sums(time_arr, fn_arr, freq_arr)
+t_brute = time.time()-t_start
+
+print 't_brute ',t_brute
+print 't_extirp ',t_extirp
+print 'len ',len(cos_fft_sum)
+
+
+for nu, cos_f, cos_b, sin_f, sin_b in zip(freq_arr, cos_fft_sum, cos_brute,
+                                          sin_fft_sum, sin_brute):
+
+    k = nu*delta*len(ttk)
+    print nu, k, cos_f/cos_b, sin_f/sin_b
+    if k>40:
+        exit()
+
+#print 'cos residuals ', np.abs(cos_compare-cos_brute).max()
+#print 'sin residuals ', np.abs(sin_compare-sin_brute).max()
+#print 'n_freq ',len(freq_arr),delta
+#for ff, cc1, cc2 in zip(freq_arr, cos_brute, cos_compare):
+#    print ff,cc1,cc2
+
