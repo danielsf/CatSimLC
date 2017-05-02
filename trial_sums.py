9f7df8f5808be6183c53f15d63ec12747ffe87ad
danielsf <scott.f.daniel@gmail.com>
2017-05-02 11:21:35 -0700

    trial_sums.py makes plots

--- trial_sums.py
+++ trial_sums.py
@@ -1,3 +1,7 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
 import numpy as np
 
 def brute_sums(time_arr, f_arr, freq_arr):
@@ -19,11 +23,11 @@ import time
 from fft import extirp_sums
 
 rng = np.random.RandomState(45)
-time_arr = rng.random_sample(5)*2.0*np.pi
+time_arr = rng.random_sample(20)*56.9
 time_arr = np.sort(time_arr)
 fn_arr = f_of_t(time_arr)
 
-delta = (time_arr.max()-0.0)/2048.0
+delta = (time_arr.max()-0.0)/(4*2048.0)
 
 t_start = time.time()
 cos_fft_sum, sin_fft_sum, ttk, hhk = extirp_sums(time_arr, fn_arr, delta)
@@ -44,13 +48,35 @@ print 't_extirp ',t_extirp
 print 'len ',len(cos_fft_sum)
 
 
+rat_arr = []
+k_arr = []
+nu_arr = []
 for nu, cos_f, cos_b, sin_f, sin_b in zip(freq_arr, cos_fft_sum, cos_brute,
                                           sin_fft_sum, sin_brute):
 
+
     k = nu*delta*len(ttk)
-    print nu, k, cos_f/cos_b, sin_f/sin_b
-    if k>70:
-        exit()
+    nu_arr.append(nu)
+    k_arr.append(k)
+    if np.abs(cos_f/cos_b-1.0) > np.abs(sin_f/sin_b-1.0):
+        rat_arr.append(cos_f/cos_b)
+    else:
+        rat_arr.append(sin_f/sin_b)
+
+
+plt.figsize = (30,30)
+plt.subplot(2,1,1)
+plt.plot(nu_arr,rat_arr)
+plt.xlabel('$\\nu$')
+plt.yscale('log')
+plt.xscale('log')
+plt.subplot(2,1,2)
+plt.plot(k_arr,rat_arr)
+plt.xlabel('k')
+plt.yscale('log')
+plt.xscale('log')
+plt.tight_layout()
+plt.savefig('sum_plot.png')
 
 #print 'cos residuals ', np.abs(cos_compare-cos_brute).max()
 #print 'sin residuals ', np.abs(sin_compare-sin_brute).max()
