61e110718ffd8beb9d3e5fa4ace4b3ab19ac5fef
danielsf <scott.f.daniel@gmail.com>
2017-05-31 19:49:15 -0700

    use linear fit to stitch together light curve segments

--- extract_lightcurve.py
+++ extract_lightcurve.py
@@ -3,6 +3,49 @@ import os
 import numpy as np
 import astropy.io.fits as fits
 
+def linear_bridge(left_x, left_y, left_s,
+                  right_x, right_y, right_s):
+
+    theta_1=(left_x/np.power(left_s,2)).sum()/(1.0/np.power(left_s,2)).sum()
+    theta_2=(right_x/np.power(right_s,2)).sum()/(1.0/np.power(right_s,2)).sum()
+    mm_denom = ((np.power(left_x,2)-left_x*theta_1)/np.power(left_s,2)).sum()
+    mm_denom += ((np.power(right_x,2)-right_x*theta_2)/np.power(right_s,2)).sum()
+
+    theta_1=(left_y/np.power(left_s,2)).sum()/(1.0/np.power(left_s,2)).sum()
+    theta_2=(right_y/np.power(right_s,2)).sum()/(1.0/np.power(right_s,2)).sum()
+    mm_num = ((left_y*left_x-left_x*theta_1)/np.power(left_s,2)).sum()
+    mm_num += ((right_y*right_x-right_x*theta_2)/np.power(right_s,2)).sum()
+
+    mm = mm_num/mm_denom
+
+    bb = ((left_y-mm*left_x)/np.power(left_s,2)).sum()
+    bb = bb/((1.0/np.power(left_s,2)).sum())
+
+    aa = ((right_y-mm*right_x)/np.power(right_s,2)).sum()
+    aa = aa/((1.0/np.power(right_s,2)).sum())
+    aa -= bb
+
+    return mm, bb, aa
+
+x=np.arange(10.0, 20.0, 1.0)
+y=x*2.3-7.2
+x2 = np.arange(40.0, 50.0, 10.0)
+y2 = x2*2.3-7.2-9.3
+
+mm, bb, aa = linear_bridge(x,y,np.ones(len(x)),
+                           x2,y2,np.ones(len(x)))
+
+print mm,bb,aa
+#mm=2.3
+#bb=-7.2
+#aa=-9.3
+chisq = np.power(y-mm*x-bb,2).sum()
+chisq += np.power(y2-mm*x2-bb-aa,2).sum()
+print 'chisq ',chisq
+
+
+overhang = 100
+
 obj_id = '009449503'
 #obj_id = '008097275'
 
@@ -20,6 +63,8 @@ flux = None
 sig = None
 
 target_flux = None
+
+line_ct = 0
 
 for file_name in list_of_files:
     if obj_id in file_name and file_name.endswith('fits'):
@@ -47,24 +92,30 @@ for file_name in list_of_files:
         valid_flux = data[1].data['PDCSAP_FLUX'][valid_dexes]
         valid_mjd = data[1].data['TIME'][valid_dexes]
         valid_sig = data[1].data['PDCSAP_FLUX_ERR'][valid_dexes]
-
         print valid_mjd.min(),valid_mjd.max()
 
         if mjd is None:
             mjd = valid_mjd
             flux = valid_flux
             sig = valid_sig
-            target_flux = valid_flux[-1]
         else:
+            mm, bb, aa = linear_bridge(mjd[-overhang:],
+                                       flux[-overhang:],
+                                       sig[-overhang:],
+                                       valid_mjd[:overhang],
+                                       valid_flux[:overhang],
+                                       valid_sig[:overhang])
+
+            with open('line_file_%d.txt' % line_ct, 'w') as out_file:
+                for t in mjd[-overhang:]:
+                    out_file.write('%e %e\n' % (t,mm*t+bb))
+                for t in valid_mjd[:overhang]:
+                    out_file.write('%e %e\n' % (t,mm*t+bb))
+            line_ct += 1
             mjd = np.append(mjd, valid_mjd)
-            #flux = np.append(flux, valid_flux)
-            flux = np.append(flux, valid_flux+target_flux-valid_flux[0])
+            flux = np.append(flux, valid_flux-aa)
             sig = np.append(sig, valid_sig)
             target_flux = flux[-1]
-
-print mjd
-print flux
-print sig
 
 valid = np.where(np.logical_not(np.isnan(mjd)))
 mjd= mjd[valid]
