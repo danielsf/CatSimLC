c801692d9d8d9b0913de0ec2fff9f0912cb3cb9b
danielsf <scott.f.daniel@gmail.com>
2017-06-02 10:09:09 -0700

    do not constantly output name of file being processed just output name of file that you failed on in extract_all_light_curves.py

--- extract_all_light_curves.py
+++ extract_all_light_curves.py
@@ -45,35 +45,40 @@ for lc_dir in in_dirs:
             fits_name = member.name
             if not fits_name.endswith('fits'):
                 continue
-            print '    trying to process',fits_name
-            obj_name = fits_name.split('/')[-1].split('-')[0]
-            out_name = os.path.join(out_dir, '%s_lc.txt' % obj_name)
-            file_obj = tar_file.extractfile(member)
-            fits_file = fits.open(file_obj)
-            mjd = fits_file[1].data['TIME']
-            flux = fits_file[1].data['PDCSAP_FLUX']
-            sig = fits_file[1].data['PDCSAP_FLUX_ERR']
-            valid_dexes = np.where(np.logical_and(np.logical_not(np.isnan(mjd)),
-                                   np.logical_and(np.logical_not(np.isnan(flux)),
-                                                  np.logical_not(np.isnan(sig)))))
 
-            mjd = mjd[valid_dexes]
-            flux = flux[valid_dexes]
-            sig = sig[valid_dexes]
+            try:
+                obj_name = fits_name.split('/')[-1].split('-')[0]
+                out_name = os.path.join(out_dir, '%s_lc.txt' % obj_name)
+                file_obj = tar_file.extractfile(member)
+                fits_file = fits.open(file_obj)
+                mjd = fits_file[1].data['TIME']
+                flux = fits_file[1].data['PDCSAP_FLUX']
+                sig = fits_file[1].data['PDCSAP_FLUX_ERR']
+                valid_dexes = np.where(np.logical_and(np.logical_not(np.isnan(mjd)),
+                                       np.logical_and(np.logical_not(np.isnan(flux)),
+                                                      np.logical_not(np.isnan(sig)))))
 
-            if out_name not in end_points:
-                end_points[out_name] = []
-            end_points[out_name].append((mjd[0], mjd[-1]))
+                mjd = mjd[valid_dexes]
+                flux = flux[valid_dexes]
+                sig = sig[valid_dexes]
 
-            if out_name in has_written:
-                mode = 'a'
-            else:
-                mode = 'w'
-                has_written.append(out_name)
+                if out_name not in end_points:
+                    end_points[out_name] = []
+                end_points[out_name].append((mjd[0], mjd[-1]))
 
-            with open(out_name, mode) as output_file:
-                for tt, ff, ss in zip(mjd,flux,sig):
-                    output_file.write('%.12e %e %e\n' % (tt, ff, ss))
+                if out_name in has_written:
+                    mode = 'a'
+                else:
+                    mode = 'w'
+                    has_written.append(out_name)
+
+                with open(out_name, mode) as output_file:
+                    for tt, ff, ss in zip(mjd,flux,sig):
+                        output_file.write('%.12e %e %e\n' % (tt, ff, ss))
+            except:
+                print 'failed on: %s' % fits_name
+                raise
+
 
         print 'processed in %s' % tar_file_name
 
