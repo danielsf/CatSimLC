bb4c6bc2fbe044b62240d7e1bac172c34650d5ff
danielsf <scott.f.daniel@gmail.com>
2017-06-05 09:50:33 -0700

    only output file name if it fails

--- assemble_final_lc.py
+++ assemble_final_lc.py
@@ -42,42 +42,45 @@ t_start = time.time()
 data_dict = {}
 
 for file_name in obj_list['lc']:
-    print 'trying to create ',file_name
-    flux = None
-    mjd = None
-    sig = None
-    last_t = -1.0
-    for in_dir in dir_list:
-        if not file_name in files_in_dir[in_dir]:
-            continue
+    try:
+        flux = None
+        mjd = None
+        sig = None
+        last_t = -1.0
+        for in_dir in dir_list:
+            if not file_name in files_in_dir[in_dir]:
+                continue
 
-        in_name = os.path.join(in_dir, file_name)
-        data = np.genfromtxt(in_name, dtype=dtype)
-        if data['t'].min() < last_t:
-            continue
+            in_name = os.path.join(in_dir, file_name)
+            data = np.genfromtxt(in_name, dtype=dtype)
+            if data['t'].min() < last_t:
+                continue
 
-        last_t = data['t'].max()
+            last_t = data['t'].max()
 
-        if flux is None:
-            flux = data['f']
-            mjd = data['t']
-            sig = data['s']
-        else:
-            flux = np.append(flux, data['f'] - data['f'][0] + flux[-1])
-            mjd = np.append(mjd, data['t'])
-            sig = np.append(sig, data['s'])
+            if flux is None:
+                flux = data['f']
+                mjd = data['t']
+                sig = data['s']
+            else:
+                flux = np.append(flux, data['f'] - data['f'][0] + flux[-1])
+                mjd = np.append(mjd, data['t'])
+                sig = np.append(sig, data['s'])
 
-    data_dict[file_name] = (mjd, flux, sig)
+        data_dict[file_name] = (mjd, flux, sig)
 
-    if (len(data_dict) >= args.write_every or
-        file_name == obj_list['lc'][-1]):
+        if (len(data_dict) >= args.write_every or
+            file_name == obj_list['lc'][-1]):
 
-        for file_name in data_dict.keys():
-            out_name = os.path.join(out_dir, file_name)
-            with open(out_name, 'w') as out_file:
-               obj_data = data_dict[file_name]
-               for (tt, ff, ss) in zip(obj_data[0], obj_data[1], obj_data[2]):
-                   out_file.write('%.12e %e %e\n' % (tt, ff, ss))
+            for file_name in data_dict.keys():
+                out_name = os.path.join(out_dir, file_name)
+                with open(out_name, 'w') as out_file:
+                   obj_data = data_dict[file_name]
+                   for (tt, ff, ss) in zip(obj_data[0], obj_data[1], obj_data[2]):
+                       out_file.write('%.12e %e %e\n' % (tt, ff, ss))
 
-        del data_dict
-        data_dict = {}
+            del data_dict
+            data_dict = {}
+    except:
+        print('failed on %s' % file_name)
+        raise
