4c7cc148eaaface1910df7e780be97c5bedd6221
danielsf <scott.f.daniel@gmail.com>
2017-06-07 15:27:59 -0700

    if only one segment, do not worry about optimizing first_segment

--- clean_by_segments.py
+++ clean_by_segments.py
@@ -56,12 +56,13 @@ def re_calibrate_lc(time_arr, flux_arr, sigma_arr, segments):
     # separate these light curves from each other and try to pick one, I am
     # just going to discard any light curve segments that have double reporting.
     first_segment = 0
-    while ((start_times[first_segment] > start_times[first_segment+1]-tol and
-            start_times[first_segment] < end_times[first_segment+1]+tol) or
-           (start_times[first_segment] > start_times[first_segment-1]-tol and
-            start_times[first_segment] < end_times[first_segment-1]-tol)):
+    if len(start_times)>1:
+        while ((start_times[first_segment] > start_times[first_segment+1]-tol and
+                start_times[first_segment] < end_times[first_segment+1]+tol) or
+               (start_times[first_segment] > start_times[first_segment-1]-tol and
+                start_times[first_segment] < end_times[first_segment-1]-tol)):
 
-        first_segment += 1
+            first_segment += 1
 
     #print '    first_segment ',first_segment
 
