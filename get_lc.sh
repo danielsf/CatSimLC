a5317a9578a274dd828d0a84d16bc98e1ab34ef1
danielsf <scott.f.daniel@gmail.com>
2017-08-10 14:58:33 -0700

    add bash script to copy stitched lc from astro-lsst-01

---
+++ get_lc.sh
@@ -0,0 +1,8 @@
+scp danielsf@astro-lsst-01.astro.washington.edu:/astro/store/pogo4/danielsf/kepler_data/all_lightcurves/cori/stitch/kplr{$1}_lc_stitched.txt workspace/validation_170705/stitched/
+
+# kplr008240861_lc.txt
+# kplr007740566_lc.txt
+# kplr002719436_lc.txt
+# kplr003549994_lc.txt
+# kplr010447902_lc.txt
+# kplr009202969_lc.txt
