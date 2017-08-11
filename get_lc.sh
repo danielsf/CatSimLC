1eec46c20827898609fb2360dc6c3e3146b01b9a
danielsf <scott.f.daniel@gmail.com>
2017-08-11 14:08:03 -0700

    get master light curve data with .sh script as well

--- get_lc.sh
+++ get_lc.sh
@@ -1,5 +1,8 @@
 scp danielsf@astro-lsst-01.astro.washington.edu:/astro/store/pogo4/danielsf/kepler_data/all_lightcurves/cori/stitch/kplr{$1}_lc_stitched.txt workspace/validation_170705/stitched/
 
+
+scp danielsf@astro-lsst-01.astro.washington.edu:/astro/store/pogo4/danielsf/kepler_data/all_lightcurves/lc_master_3/kplr{$1}_lc.txt workspace/validation_170705/lc_master/
+
 # kplr008240861_lc.txt
 # kplr007740566_lc.txt
 # kplr002719436_lc.txt
