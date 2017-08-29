1fab49ea4d3539edde2986ba6f558bc5aa9593c3
danielsf <scott.f.daniel@gmail.com>
2017-08-29 09:48:15 -0700

    get_lc.sh works off of the 170824 iteration

--- get_lc.sh
+++ get_lc.sh
@@ -1,7 +1,7 @@
-scp danielsf@astro-lsst-01.astro.washington.edu:/astro/store/pogo4/danielsf/kepler_data/all_lightcurves/cori/stitch_170811/kplr{$1}_lc_stitched.txt workspace/validation_170818/stitched/
+scp danielsf@astro-lsst-01.astro.washington.edu:/astro/store/pogo4/danielsf/kepler_data/all_lightcurves/cori/stitch_170824/kplr{$1}_lc_stitched.txt workspace/validation_170824/stitched/
 
 
-scp danielsf@astro-lsst-01.astro.washington.edu:/astro/store/pogo4/danielsf/kepler_data/all_lightcurves/lc_master_3/kplr{$1}_lc.txt workspace/validation_170818/lc_master/
+scp danielsf@astro-lsst-01.astro.washington.edu:/astro/store/pogo4/danielsf/kepler_data/all_lightcurves/lc_master_3/kplr{$1}_lc.txt workspace/validation_170824/lc_master/
 
 # kplr008240861_lc.txt
 # kplr007740566_lc.txt
